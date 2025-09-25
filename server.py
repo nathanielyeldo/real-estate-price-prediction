f2fc46524c4f38da99ffd031440010dc585afcf1
Nathaniel Yeldo <nathanielyeldo22@gmail.com>
2025-09-24 21:12:37 -0400

    fix: configure Flask to serve frontend from /templates and /static

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import util
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Serve frontend
 @app.route("/")
